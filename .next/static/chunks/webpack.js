d4980b24e6a688246f60035278afe212a0bb7e41
Siddharth Mirchandani <88209108+SidMirchandani@users.noreply.github.com>
2025-09-21 16:37:39 +0000

    updated gitignore and tsconfig to ignore .bak files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d6953a2e0c235340"; }
+/******/ 		__webpack_require__.h = function() { return "e79c30a54fccbd4b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
