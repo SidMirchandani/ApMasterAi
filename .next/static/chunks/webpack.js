8efe43256e39da8e785e108b4983f9fd182b00f7
Siddharth Mirchandani <88209108+SidMirchandani@users.noreply.github.com>
2025-09-22 05:31:53 +0000

    Fixed Vercel Deployment Issue - Try 11

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ddf70ba1e538a5e6"; }
+/******/ 		__webpack_require__.h = function() { return "1caf2c85dd8000d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
