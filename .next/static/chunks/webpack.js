c1feb9b32f31b5b8aed356f3938aedd17771f6d0
Siddharth Mirchandani <88209108+SidMirchandani@users.noreply.github.com>
2025-09-22 04:37:45 +0000

    Fixed Vercel Missing Firebase Config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "500757c8196d121b"; }
+/******/ 		__webpack_require__.h = function() { return "2cf92af6271b5680"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
