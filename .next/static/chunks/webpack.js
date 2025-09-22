8fc54c375cbc0c7424a32dc8bc7a218e56245c78
Siddharth Mirchandani <88209108+SidMirchandani@users.noreply.github.com>
2025-09-22 04:06:46 +0000

    Works on Vercel - Firebase Database Working Properly

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8c29539b05a5bdee"; }
+/******/ 		__webpack_require__.h = function() { return "500757c8196d121b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
