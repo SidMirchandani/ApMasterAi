370feb373c5ac0ea00378984955a2476d967512c
Siddharth Mirchandani <88209108+SidMirchandani@users.noreply.github.com>
2025-10-10 18:19:52 +0000

    AP Master - MVP Version 2 - UI LockIn - Works for ALL Devices
    1. Fixed sync issues between courses and dashboard
    2. Made UI consistent across pages and devices.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eb37034df8d7e6ad"; }
+/******/ 		__webpack_require__.h = function() { return "6e5f847882d6db67"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
